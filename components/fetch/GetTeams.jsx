f8bf1100b4813226353569120ae77b1a53eeb7d2
Andrew Virts <asvirts@outlook.com>
2023-10-15 20:36:33 -0500

    did i do it?

--- components/fetch/GetTeams.jsx
+++ components/fetch/GetTeams.jsx
@@ -1,9 +1,18 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-export default async function GetTeams(props: any) {
+export default async function GetTeams(props) {
   const cookieStore = cookies();
   const supabase = createServerComponentClient({ cookies: () => cookieStore });
+
+  async function upvote(rank, id) {
+    let newRank = rank + 1;
+
+    const { error } = await supabase
+      .from("teams")
+      .update({ ranking: { newRank } })
+      .eq("id", id);
+  }
 
   const { data: teams } = await supabase
     .from("teams")
@@ -17,7 +26,10 @@ export default async function GetTeams(props: any) {
           <button className="inline-flex items-center justify-center rounded-md px-4 py-1 text-white bg-slate-500 me-1">
             Down
           </button>
-          <button className="inline-flex items-center justify-center rounded-md px-4 py-1 text-white bg-slate-500 me-1">
+          <button
+            className="inline-flex items-center justify-center rounded-md px-4 py-1 text-white bg-slate-500 me-1"
+            onClick={(e) => this.upvote({team.ranking}, {team.id})}
+          >
             Up
           </button>
           Rank: {team.ranking} | Team: {team.name}
