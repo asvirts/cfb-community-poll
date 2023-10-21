ac194044d72c3eb21fcd90e5f3cf536e9aac7a65
Andrew Virts <asvirts@outlook.com>
2023-10-21 14:27:05 -0500

    accessed supabase supa easy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    export let data;
+  </script>
+
+  <ul>
+    {#each data.teams as teams}
+      <li>{teams.name}</li>
+    {/each}
+  </ul>
