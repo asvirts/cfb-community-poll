431a373a9db29d83019ce7ccb82dfca47c680273
Andrew Virts <asvirts@outlook.com>
2024-02-21 12:43:50 -0600

    reset

--- src/lib/supabaseClient.js
+++
@@ -1,3 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient('https://lqddsfeluzxoxxtnjxqv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxZGRzZmVsdXp4b3h4dG5qeHF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTczODAzODAsImV4cCI6MjAxMjk1NjM4MH0.qg9ftTAKN48O00ugeUD-9VXZBF9MKuaHX-2Wx5biz88')
--- src/routes/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { supabase } from "$lib/supabaseClient";
-
-  export async function load() {
-    const { data } = await supabase.from("teams").select().order('ranking', { ascending: false });
-    return {
-      teams: data ?? [],
-    };
-  }
